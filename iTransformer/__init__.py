33b8a32e023ebf9228464838a17c2d5547e7e904
lucidrains <lucidrains@gmail.com>
2024-01-30 08:37:16 -0800

    address https://github.com/lucidrains/iTransformer/issues/22

--- iTransformer/__init__.py
+++ iTransformer/__init__.py
@@ -6,5 +6,3 @@ from iTransformer.iTransformer import (
 from iTransformer.iTransformer2D import iTransformer2D
 
 from iTransformer.iTransformerFFT import iTransformerFFT
-
-from iTransformer.iTransformerNormConditioned import iTransformerNormConditioned
